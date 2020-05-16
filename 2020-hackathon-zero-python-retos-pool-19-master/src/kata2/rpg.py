441e4f293dc66d55636373bf7eb7652cddfa810b
luciavicario <luciavicaguerrero@hotmail.com>
2020-05-16 19:11:39 +0200

    Añadir Reto2

--- 2020-hackathon-zero-python-retos-pool-19-master/src/kata2/rpg.py
+++ 2020-hackathon-zero-python-retos-pool-19-master/src/kata2/rpg.py
@@ -3,11 +3,18 @@
 import random
 import string
 
-def RandomPasswordGenerator(passLen=10):
-    #
-    #
-    
-    #
-    #
+def RandomPasswordGenerator(passLen = 10):
+    total_posibilities = string.ascii_letters + string.digits + string.punctuation
+    print (total_posibilities)
 
-    return ""
+    password = ""
+    for positions in range(passLen):
+        password = password + random.choice(total_posibilities)
+    
+    #password = random.sample(total_posibilities,passLen)
+    return password
+
+password = RandomPasswordGenerator(10)
+#password = ''.join(password)
+print(password)
+   
